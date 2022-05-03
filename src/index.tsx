efa7478b284798ad594b2f8cdf09ae5a95ee8872
Ali Faris <alihcompiler@gmail.com>
2022-05-03 22:16:41 +0300

    install tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./index.css"
 
 ReactDOM.render(
     <React.StrictMode>
-        <h1>wbox-collection-tailwindcss</h1>
+    <h1 className="text-3xl font-bold underline w-full text-center">
+        wbox-collection-tailwindcss
+    </h1>
     </React.StrictMode>,
     document.getElementById('root'),
 );
